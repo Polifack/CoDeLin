8ec2e286af73fac52c78229c20f3544bde754bfd
Poli <diegoroca.ordes@gmail.com>
2023-08-01 10:12:50 +0200

    Fix hexatag implementation

--- codelin/encs/enc_deps/hexatag.py
+++ codelin/encs/enc_deps/hexatag.py
@@ -1,8 +1,9 @@
 from codelin.encs.abstract_encoding import ADEncoding
 from codelin.models.deps_label import D_Label
 from codelin.models.deps_tree import D_Tree
+from codelin.encs.constituent import C_Tetratag
 from codelin.models.const_tree import C_Tree
-from codelin.utils.constants import D_NONE_LABEL, D_2P_GREED, D_2P_PROP
+from codelin.utils.constants import D_NONE_LABEL, D_2P_GREED, D_2P_PROP, C_NONE_LABEL
 from codelin.models.linearized_tree import LinearizedTree
 
 class D_HexatagEncoding(ADEncoding):
@@ -11,87 +12,19 @@ class D_HexatagEncoding(ADEncoding):
         super().__init__(separator)
 
     def __str__(self):
-        return "Dependency Bracketing Hexa-Tags Encoding"
+        return "Dependency Hexa-Tags Encoding"
 
     def encode(self, dep_tree):
-        bht_tree = D_Tree.to_bht(dep_tree)
-        inorder_traversal = []
-        C_Tree.inorder(bht_tree, lambda x: inorder_traversal.append(x))
-
-        def get_hexalabel(node):
-            '''
-            Returns the hexa-tag label of the node:
-
-                'r'  =>  node is terminal and right child of its parent
-                'l'  =>  node is terminal and left child of its parent
-                
-                'RR' =>  node is non-terminal right child and the label of its parent is 'R'
-                'RL' =>  node is non-terminal right child and the label of its parent is 'L'
-                
-                'LR' =>  node is non-terminal left child and the label of its parent is 'R'
-                'LL' =>  node is non-terminal left child and the label of its parent is 'L'
-            '''
-
-            if node.is_terminal():
-                if node.is_right_child():
-                    return "r"
-                else:
-                    return "l"
-            else:
-                # root node case
-                if node.parent is None:
-                    return "LL"
-
-                if node.is_right_child():
-                    return "R" + node.label
-                else:
-                    return "L" + node.label
-        
-        labels = {}
-        current_label = ""
-        
-        trav_labels = [n.label for n in inorder_traversal]
-        print(trav_labels)
-        
-        for node in inorder_traversal:
-            hexalabel = get_hexalabel(node)
-            current_label+=hexalabel
-            if node.is_terminal():
-                labels[int(node.label)]=current_label
-                current_label = ""
-            
-        # sort
-        labels = dict(sorted(labels.items(), key=lambda item: item[0]))
-        # to list
-        labels = list(labels.values())
-        lin_tree = LinearizedTree(dep_tree.get_words(), dep_tree.get_postags(), dep_tree.get_feats(), labels, 0)
+        # to encode hexatags we employ tetratagging into a bht converted dependency tree
+        # the reltype encoding will be dealt by the unary chain encoding mechanism
+        bht_tree = D_Tree.to_bht(dep_tree, include_reltype=True)
+        tagger = C_Tetratag(separator=self.separator, unary_joiner="+", mode="inorder", binary_marker="[b]")
+        lin_tree = tagger.encode(bht_tree)        
         return lin_tree
         
     def decode(self, lin_tree):
-        def get_hexalabels(label):
-            '''
-            given a concatenation of hexa-tags, returns a list of hexa-tags
-            '''
-            hexalabels = []
-            for i in range(0, len(label), 2):
-                hexalabels.append(label[i:i+2])
-            return hexalabels
+        tagger = C_Tetratag(separator=self.separator, unary_joiner="+", mode="inorder", binary_marker="[b]")
+        bht_tree = tagger.decode(lin_tree)
+        dectree = D_Tree.from_bht(bht_tree)
 
-        for word, postag, features, label in lin_tree.iterrows():
-            print(word, postag, features, label)
-            hexalabels = get_hexalabels(label)
-            for hl in hexalabels:
-                if hl == "r":
-                    print("right child")
-                elif hl == "l":
-                    print("left child")
-                elif hl == "RR":
-                    print("non-terminal right child, parent label is R")
-                elif hl == "RL":
-                    print("non-terminal right child, parent label is L")
-                elif hl == "LR":
-                    print("non-terminal left child, parent label is R")
-                elif hl == "LL":
-                    print("non-terminal left child, parent label is L")
-                else:
-                    print("unknown hexa-tag label")
+        return dectree
